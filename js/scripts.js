214a2b24994cc87f689055be7f891ee3efdd4346
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2020-12-25 21:44:27 +0500

    Fixsed js file

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,7 @@ var createElement = function (element, elementClass, text) {
   return newElement;
 };
 
+
 var contactArray = [];
 var relationArray = [];
 
@@ -34,71 +35,111 @@ var elList = $_('.js-contacts');
 var elDatalist = document.querySelector('#relationships-list');
 elName.focus();
 
+
 elForm.addEventListener('submit', function(evt) {
   evt.preventDefault();
 
-  for(var i = 0; i < contactArray.length; i++) {
-    elNumber.classList.remove('is-invalid');
-    if(elNumber.value === contactArray[i].number) {
+  /* ==============  IS INVALID  =================== */
+  elNumber.classList.remove('is-invalid');
+  for (var i = 0; i < contactArray.length; i++){
+    if (contactArray[i].number === elNumber.value){
       elNumber.classList.add('is-invalid');
-      console.log(`salooom`);
       return;
     }
   }
+
   contactArray.push({
-    name: elName.value,
-    rel: elRelationship.value,
+    text: elName.value,
+    reletion: elRelationship.value,
     number: elNumber.value
   });
 
-
+  /* ========  Creat Option  ========== */
   if(!relationArray.includes(elRelationship.value)) {
     relationArray.push(elRelationship.value);
   }
-
   elDatalist.innerHTML = '';
   for(var i = 0; i < relationArray.length; i++) {
-
     var elOption = document.createElement('option');
     elOption.value = relationArray[i];
     elOption.textContent = relationArray[i];
-
     elDatalist.appendChild(elOption);
   }
 
   elList.innerHTML = '';
+  var elItemsFragment = document.createDocumentFragment();
+  var i = 0;
+
   for(var i = 0; i < contactArray.length; i++) {
 
-    var listItem = document.createElement('li');
-    listItem.classList.add('list-group-item');
+    var listItem = createElement('li', 'list-group-item js-contact');
+    var contactTitle = createElement('h3', 'h5 text-truncate js-contact__name');
+    contactTitle.textContent = contactArray[i].text;
+    var btnDel = createElement('button', 'btn btn-sm btn-danger', 'X');
+    btnDel.dataset.id = i;
+    var relativeness = createElement('p', 'small mb-1 js-contact__relationship');
+    relativeness.textContent = contactArray[i].reletion;
+    var phoneNumber = createElement('a', 'js-contact__phone');
+    phoneNumber.textContent = contactArray[i].number;
+    phoneNumber.href = `tel: ${contactArray[i].number}`;
+    var div = createElement('div', 'd-flex justify-content-between');
 
-    var nameTitle = document.createElement('h3');
-    nameTitle.classList.add('h5','text-truncate');
-    nameTitle.textContent = contactArray[i].name;
+    div.appendChild(contactTitle);
+    div.appendChild(btnDel);
+    listItem.appendChild(div);
+    listItem.appendChild(relativeness);
+    listItem.appendChild(phoneNumber);
+    elItemsFragment.appendChild(listItem);
 
-    var relation = document.createElement('p');
-    relation.classList.add('small', 'mb-1');
-    relation.textContent = contactArray[i].rel;
-
-    var numberLink = document.createElement('a');
-    numberLink.href = `tel: ${contactArray.number}`;
-    numberLink.textContent = contactArray[i].number;
-
-    listItem.appendChild(nameTitle);
-    listItem.appendChild(relation);
-    listItem.appendChild(numberLink);
-
-    elList.appendChild(listItem);
+    elName.focus();
+    elName.value = '';
+    elRelationship.value = '';
+    elNumber.value = '';
   }
-  elName.focus();
-  elName.value = '';
-  elRelationship.value = '';
-  elNumber.value = '';
+  elList.appendChild(elItemsFragment);
 });
 
 
-//  <li class="list-group-item js-contact">
-//    <h3 class="h5 text-truncate js-contact__name">Lorem ipsum dolo</h3>
-//    <p class="small mb-1 js-contact__relationship">Qarindosh</p>
-//    <a href="tel:+998909213711" class="js-contact__phone">+998909213711</a>
-//  </li>
+/* ========================================================= */
+
+elList.addEventListener('click', function(evt) {
+
+  if(evt.target.matches('.btn-danger')){
+    contactArray.splice(evt.target.dataset.id, 1);
+  }
+
+
+  var elItemsFragment = document.createDocumentFragment();
+  var i = 0;
+
+  elList.innerHTML = '';
+  for(var i = 0; i < contactArray.length; i++) {
+
+    var listItem = createElement('li', 'list-group-item js-contact');
+    var contactTitle = createElement('h3', 'h5 text-truncate js-contact__name');
+    contactTitle.textContent = contactArray[i].text;
+    var btnDel = createElement('button', 'btn btn-sm btn-danger', 'X');
+    btnDel.dataset.id = i;
+    var relativeness = createElement('p', 'small mb-1 js-contact__relationship');
+    relativeness.textContent = contactArray[i].reletion;
+    var phoneNumber = createElement('a', 'js-contact__phone');
+    phoneNumber.textContent = contactArray[i].number;
+    phoneNumber.href = `tel: ${contactArray[i].number}`;
+    var div = createElement('div', 'd-flex justify-content-between');
+
+    div.appendChild(contactTitle);
+    div.appendChild(btnDel);
+    listItem.appendChild(div);
+    listItem.appendChild(relativeness);
+    listItem.appendChild(phoneNumber);
+    elItemsFragment.appendChild(listItem);
+
+    elName.focus();
+    elName.value = '';
+    elRelationship.value = '';
+    elNumber.value = '';
+
+  }
+  elList.appendChild(elItemsFragment)
+  console.log(contactArray);
+});
